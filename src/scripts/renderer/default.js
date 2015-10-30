be83681e85f60f725833c2f5f0bec4e3d9949e72
Tyler Anderson <anderson.t.e@gmail.com>
2015-10-30 13:59:35 -0700

    tidying a bit

--- src/scripts/renderer/default.js
+++ src/scripts/renderer/default.js
@@ -5,7 +5,15 @@ Tyler Anderson 2015
 const jQuery = require('jQuery');
 const $ = jQuery;
 const numeral = require('numeral');
+
 console.log('renderer/default.js is go');
+
+const context = new AudioContext(),
+      gainNode = context.createGain();
+
+function changeMasterVolume(percent) {
+    
+}
 
 nx.transmit = function(data){ console.log(
     {
